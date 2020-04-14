512ce30b73e91c1241005beae1f47e7a2e5a2e86
Frederik Hansen <fhh@ctrl-it.dk>
2020-04-14 13:14:25 +0200

    Version 2

---
+++ EggscuseMeV20/app/src/main/java/com/example/eggscusemev20/EggTimerContract.java
@@ -0,0 +1,11 @@
+package com.example.eggscusemev20;
+
+public interface EggTimerContract {
+
+
+}
+
+interface EggTimerView{
+    void onCountDown();
+    void onEggTimerStopped();
+}
---
+++ EggscuseMeV20/app/src/main/java/com/example/eggscusemev20/EggTimerPresenter.java
@@ -0,0 +1,5 @@
+package com.example.eggscusemev20;
+
+public class EggTimerPresenter {
+
+}
